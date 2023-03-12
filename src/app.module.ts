43797498db0e4fc71271a4377f7dd0b6c38dc7cd
Kureans <leonardtengys@gmail.com>
2023-03-12 13:46:01 +0800

    Set up mysql db connection via typeORM, define url entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { Url } from './url/url.entity';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [UrlModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'URL.mysql',
+      entities: [Url],
+      synchronize: true,
+    }),
+    UrlModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
