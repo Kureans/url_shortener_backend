07101b0f443fe24c48bba16b4e6437703ecd87bd
Kureans <leonardtengys@gmail.com>
2023-03-12 17:01:49 +0800

    Implement shortenUrl logic

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Url } from './url.entity';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class UrlService {
-  shortenUrl(url: string) {
-    return url;
+  constructor(
+    @InjectRepository(Url)
+    private repo: Repository<Url>,
+  ) {}
+
+  async shortenUrl(longUrl: string) {
+    const urlCode = nanoid(8);
+    const rootPath = 'http://localhost:3000';
+    try {
+      let url = await this.repo.findOneBy({ longUrl });
+      if (url) return url.shortUrl;
+
+      const shortUrl = `${rootPath}/${urlCode}`;
+      url = this.repo.create({
+        urlCode,
+        longUrl,
+        shortUrl,
+      });
+
+      this.repo.save(url);
+      return url.longUrl;
+    } catch (error) {
+      throw new UnprocessableEntityException('Server ran into an error');
+    }
   }
 }
